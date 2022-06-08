6d21f05c6606213a61b5705eff4416be6944ff27
zhiwi <zhiwilliam@gmail.com>
2022-06-08 15:23:10 -0400

    docker

--- pipelines/Jenkinsfile.cicd.groovy
+++ pipelines/Jenkinsfile.cicd.groovy
@@ -22,7 +22,7 @@ pipeline {
         }
 
         stage("sbt build") {
-            agent { docker { image "hseeberger/scala-sbt:8u222_1.3.5_2.13.1" }}
+            //agent { docker { image "hseeberger/scala-sbt:8u222_1.3.5_2.13.1" }}
             steps {
                 sh "sbt assembly test publish"
             }
