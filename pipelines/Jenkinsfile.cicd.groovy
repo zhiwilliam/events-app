c15bf592ac0eb9ba48a77e740cbdc86ddfaf3dd3
zhiwi <zhiwilliam@gmail.com>
2022-06-09 16:00:06 -0400

    sbt

--- pipelines/Jenkinsfile.cicd.groovy
+++ pipelines/Jenkinsfile.cicd.groovy
@@ -22,7 +22,7 @@ pipeline {
         }
 
         stage("sbt build") {
-            agent { docker { image "hseeberger/scala-sbt:8u222_1.3.5_2.13.1" }}
+            agent { docker { image "wzhi-docker-sbt:0.1.0" }}
             stages {
                 stage('Compile') {
                     steps {
